ec2103be9321b56646b30c428709916e34dc989e
nicli2000 <wpei_li@hotmail.com>
2023-12-14 20:23:26 +1300

    hasWon() method updated

--- TicTacToe/src/Board.java
+++ TicTacToe/src/Board.java
@@ -52,7 +52,7 @@ public class Board {
 		
 		 // TODO: Check if the player has 3 in the playerCol.
 		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		if(cells[playerCol][0].content == thePlayer && cells[playerCol][1].content == thePlayer && cells[playerCol][2].content == thePlayer)
+		if(cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer)
 			return true;
 		
 		
@@ -62,7 +62,7 @@ public class Board {
 		 
 		
 		// TODO: Check the diagonal in the other direction
-		if(cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[0][0].content == thePlayer)
+		if(cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[2][0].content == thePlayer)
 			return true;
 		
 
